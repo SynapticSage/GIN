a3ec3ea6bb2cd88ffac8292924b8c3b6e17f4a03
Ryan Young <ryoung@brandeis.edu>
2024-07-23 00:43:54 -0400

    script to jupy fix

--- scripts/script2jupy.sh
+++ scripts/script2jupy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 script_folder=$(dirname $(realpath $0))
 py_file=${2:-"./Pyrfume_Floral.py"}
-output_file="${notebook_file%.py}.ipynb"
+output_file="${py_file%.py}.ipynb"
 echo "Converting $1 to jupyter notebook at $script_folder"
 
 # Corrected the assignment of jupytext path
@@ -19,5 +19,6 @@ $jupytext --to notebook $py_file
 echo "Conversion complete. Jupyter notebook saved in $script_folder"
 
 # Move to appropriate folder
+echo cp ../notebook/$output_file ../notebook/${output_file}.bak.$(date +%Y%m%d%H%M%S)
+cp ../notebook/$output_file ../notebook/${output_file}.bak.$(date +%Y%m%d%H%M%S)
 mv $output_file ../notebook/
-
